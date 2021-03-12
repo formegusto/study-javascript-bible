1087d165951230a47205a67ef9bcefbd44375873
formegusto <formegusto@gmail.com>
2021-03-12 12:21:09 +0900

    let

---
+++ 12_let_const_blocklevelscope/index.js
@@ -0,0 +1,54 @@
+var x = 1;
+var y = 1;
+
+// var 키워드로 선언된 변수는 같은 스코프 내에서 중복 선언을 허용한다.
+// 초기화문이 있는 변수 선언문은 자바스크립트 엔진에 의해 var 키워드가 없는 것처럼 동작한다.
+var x = 100;
+//초기화문이 없는 변수 선언문은 무시된다.
+var y;
+
+console.log(x); // 100
+console.log(y); // 1
+
+var x = 1;
+if (true) {
+  var x = 10;
+}
+console.log(x); // 10
+
+var i = 10;
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log(i); // 5
+
+// let 키워드
+let barLet = 5;
+// let이나 const 키워드로 선언한 변수는
+// 같은 스코프 내에서 중복 선언을 허용하지 않는다.
+// let barLet = 10; // SyntaxError: Identifier 'barLet' has already been declared
+
+let foo = 1; // 전역 변수
+{
+  let foo = 2; // 지역 변수
+  let bar = 3; // 지역 변수
+}
+console.log(foo); // 1
+// console.log(bar); // ReferenceError: bar is not defined
+
+console.log(fooTwo); // // ReferenceError: Cannot access 'fooTwo' before initialization
+let fooTwo; // 변수 선언문에서 초기화 돤계가 실행된다.
+console.log(fooTwo); // undefined
+fooTwo = 1; // 할당문에서 할당 단계가 실행된다.
+console.log(fooTwo);
+
+let fooThree = 1; // 전역 변수
+{
+  console.log(fooThree); // ReferenceError: Cannot access 'fooThree' before initialization
+  let fooThree = 2;
+}
+// -----
+let fooThree = 1; // 전역 변수
+{
+  console.log(fooThree); // 선언 단계를 거치지 않아 초기화도 필요없으므로 전역 변수를 참조한다. (scope chain)
+}
