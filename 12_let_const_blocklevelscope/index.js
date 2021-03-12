96e5b81785f323e08e1292e5a029318624810f1a
formegusto <formegusto@gmail.com>
2021-03-12 15:49:21 +0900

    const

--- 12_let_const_blocklevelscope/index.js
+++ 12_let_const_blocklevelscope/index.js
@@ -36,19 +36,60 @@ let foo = 1; // 전역 변수
 console.log(foo); // 1
 // console.log(bar); // ReferenceError: bar is not defined
 
-console.log(fooTwo); // // ReferenceError: Cannot access 'fooTwo' before initialization
+// console.log(fooTwo); // // ReferenceError: Cannot access 'fooTwo' before initialization
 let fooTwo; // 변수 선언문에서 초기화 돤계가 실행된다.
 console.log(fooTwo); // undefined
 fooTwo = 1; // 할당문에서 할당 단계가 실행된다.
 console.log(fooTwo);
 
 let fooThree = 1; // 전역 변수
-{
-  console.log(fooThree); // ReferenceError: Cannot access 'fooThree' before initialization
-  let fooThree = 2;
-}
+// {
+//   console.log(fooThree); // ReferenceError: Cannot access 'fooThree' before initialization
+//   let fooThree = 2;
+// }
 // -----
-let fooThree = 1; // 전역 변수
+// let fooThree = 1; // 전역 변수
+// {
+//   console.log(fooThree); // 선언 단계를 거치지 않아 초기화도 필요없으므로 전역 변수를 참조한다. (scope chain)
+// }
+
+// const 키워드
+// const fooConst; // SyntaxError: Missing initializer in const declaration
+
 {
-  console.log(fooThree); // 선언 단계를 거치지 않아 초기화도 필요없으므로 전역 변수를 참조한다. (scope chain)
+  const fooConst = 1;
+  // fooConst = 2; // TypeError: Assignment to constant variable.
+}
+
+{
+  // 세전가격
+  let preTaxPrice = 100;
+
+  // 세후 가격
+  // 0.1의 의미를 명확히 알기 어렵기 때문에 가독성이 좋지 않다.
+  let afterTaxPrice = preTaxPrice + preTaxPrice * 0.1;
+  console.log(afterTaxPrice);
+}
+
+{
+  // 세율을 의미하는 0.1은 변경할 수 없는 상수로서 사용될 값이다.
+  // 변수 이름을 대문자로 선언해 상수임을 명확히 나타낸다.
+  const TAX_RATE = 0.1;
+
+  // 세전 가격
+  let preTaxPrice = 100;
+
+  // 세후 가격
+  let afterTaxPrice = preTaxPrice + preTaxPrice * TAX_RATE;
+
+  console.log(afterTaxPrice);
+}
+
+{
+  const person = {
+    name: "Lee",
+  };
+
+  person.name = "Kim";
+  console.log(person.name);
 }
